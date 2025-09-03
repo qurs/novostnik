e922b49403cf5a42cb85aa37916747e03cd8d678
Ivan <vanya.schetinsky@yandex.ru>
2025-09-03 19:32:16 +0300

    Пробую пофиксить билд на Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+// @ts-expect-error: No declaration file for module
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   images: {
@@ -6,6 +8,12 @@ const nextConfig: NextConfig = {
       new URL('https://i.imgur.com/**'),
       new URL('https://um8fyckwqv.ufs.sh/f/**'),
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()]
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config
+  },
 };
 export default nextConfig;
