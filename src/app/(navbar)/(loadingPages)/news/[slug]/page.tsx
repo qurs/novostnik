cd79f11090838132812dd508f8e3a6d08807a3bc
Ivan <vanya.schetinsky@yandex.ru>
2025-09-03 19:34:57 +0300

    feat: add metadata generation for news pages based on slug

--- src/app/(navbar)/(loadingPages)/news/[slug]/page.tsx
+++ src/app/(navbar)/(loadingPages)/news/[slug]/page.tsx
@@ -3,6 +3,27 @@ import { News } from "@/generated/client"
 import { notFound } from "next/navigation"
 import PageContent from "@/components/PageContent"
 import { fetchNewsFromDBBySlug } from "@/actions/db"
+import { Metadata } from "next"
+
+export async function generateMetadata({ 
+	params 
+}: {
+	params: Promise<{ slug: string }>
+}): Promise<Metadata> {
+	const { slug } = await params
+	const news: News | null = await fetchNewsFromDBBySlug(slug)
+	if (!news) {
+		return notFound()
+	}
+
+	return {
+		title: news.title,
+		description: news.title,
+		openGraph: {
+			images: news.image,
+		}
+	}
+}
 
 export default async function NewsPage({ 
 	params 
